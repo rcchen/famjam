69a7b8ceb199e183d87d8d8ba20ebfb532ac9952
Roger Chen <rogerthechen@gmail.com>
2016-04-28 19:48:02 -0700

    Remove unnecessary React related files, add JSONHelper for deserialization

--- famjam-app/FamJam/Pods/Target Support Files/CodePush/CodePush-umbrella.h
+++
@@ -1,7 +0,0 @@
-#import <UIKit/UIKit.h>
-
-#import "CodePush.h"
-
-FOUNDATION_EXPORT double CodePushVersionNumber;
-FOUNDATION_EXPORT const unsigned char CodePushVersionString[];
-
--- famjam-app/FamJam/Pods/Target Support Files/Pods-FamJam/Pods-FamJam-frameworks.sh
+++ famjam-app/FamJam/Pods/Target Support Files/Pods-FamJam/Pods-FamJam-frameworks.sh
@@ -85,11 +85,9 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/CodePush/CodePush.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/React/React.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/JSONHelper/JSONHelper.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/CodePush/CodePush.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/React/React.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/JSONHelper/JSONHelper.framework"
 fi
