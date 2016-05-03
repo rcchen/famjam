63e44cbb54f816d70c720225098b41eec4d73e11
Roger Chen <rogerthechen@gmail.com>
2016-05-02 21:47:42 -0700

    Add endpoint for participants

--- famjam-server/build/routes/api.js
+++ famjam-server/build/routes/api.js
@@ -1,42 +1,41 @@
 "use strict";
-var aws = require("aws-sdk");
-var bcrypt = require("bcrypt");
-var bodyParser = require("body-parser");
-var express = require("express");
-var jsonwebtoken = require("jsonwebtoken");
-var multer = require("multer");
-var uuid = require("node-uuid");
-var multerS3 = require("multer-s3");
-var config_1 = require("../app/config");
-var middleware_1 = require("../middleware");
-var models_1 = require("../models");
+const aws = require("aws-sdk");
+const bcrypt = require("bcrypt");
+const bodyParser = require("body-parser");
+const express = require("express");
+const jsonwebtoken = require("jsonwebtoken");
+const multer = require("multer");
+const uuid = require("node-uuid");
+const multerS3 = require("multer-s3");
+const config_1 = require("../app/config");
+const middleware_1 = require("../middleware");
+const models_1 = require("../models");
 exports.api = express();
-exports.api.use(bodyParser.json());
 aws.config.region = "us-west-2";
-var s3 = new aws.S3();
-exports.api.post("/users", function (req, res) {
-    var username = req.body.username;
-    var attributes = {
+const s3 = new aws.S3();
+exports.api.post("/users", (req, res) => {
+    const username = req.body.username;
+    const attributes = {
         displayName: req.body.displayName
     };
-    models_1.User.findOne({ username: username }, function (err, user) {
+    models_1.User.findOne({ username }, (err, user) => {
         if (user)
             return res.sendStatus(409);
-        bcrypt.hash(req.body.password, 10, function (err, password) {
-            new models_1.User({ username: username, password: password, attributes: attributes }).save(function (err, user) {
+        bcrypt.hash(req.body.password, 10, (err, password) => {
+            new models_1.User({ username, password, attributes }).save((err, user) => {
                 res.json(user);
             });
         });
     });
 });
-exports.api.post("/authenticate", function (req, res) {
-    var username = req.body.username;
-    models_1.User.findOne({ username: username }, "password", function (err, user) {
+exports.api.post("/authenticate", bodyParser.json(), (req, res) => {
+    const username = req.body.username;
+    models_1.User.findOne({ username }, "password", (err, user) => {
         if (user !== null) {
-            bcrypt.compare(req.body.password, user.password, function (err, authenticated) {
+            bcrypt.compare(req.body.password, user.password, (err, authenticated) => {
                 if (authenticated) {
-                    var uid = user._id;
-                    jsonwebtoken.sign({ uid: uid }, config_1.config.secret, {}, function (token) {
+                    const uid = user._id;
+                    jsonwebtoken.sign({ uid }, config_1.config.secret, {}, (token) => {
                         return res.json(token);
                     });
                 }
@@ -47,93 +46,93 @@ exports.api.post("/authenticate", function (req, res) {
         }
     });
 });
-exports.api.get("/users", middleware_1.authorizeToken, function (req, res) {
-    models_1.User.find({}, function (err, users) {
+exports.api.get("/users", middleware_1.authorizeToken, (req, res) => {
+    models_1.User.find({}, (err, users) => {
         res.json(users);
     });
 });
-exports.api.get("/me", middleware_1.authorizeToken, function (req, res) {
-    var uid = req.authenticatedUser._id;
+exports.api.get("/me", middleware_1.authorizeToken, (req, res) => {
+    const uid = req.authenticatedUser._id;
     models_1.User.findById(uid)
         .populate("families")
-        .exec(function (err, user) {
+        .exec((err, user) => {
         if (err)
             return res.status(500).json(err);
         res.json(user);
     });
 });
-exports.api.get("/families", middleware_1.authorizeToken, function (req, res) {
+exports.api.get("/families", middleware_1.authorizeToken, (req, res) => {
     models_1.Family.find({
         attributes: {
             displayName: req.query["displayName"]
         }
-    }, function (err, families) {
+    }, (err, families) => {
         res.json(families);
     });
 });
-exports.api.post("/families", middleware_1.authorizeToken, function (req, res) {
-    var uid = req.authenticatedUser._id;
+exports.api.post("/families", bodyParser.json(), middleware_1.authorizeToken, (req, res) => {
+    const uid = req.authenticatedUser._id;
     new models_1.Family({
         attributes: {
             displayName: req.body["displayName"]
         },
         members: [uid]
-    }).save(function (err, family) {
-        models_1.User.findById(uid, function (err, user) {
+    }).save((err, family) => {
+        models_1.User.findById(uid, (err, user) => {
             user.families.push(family._id);
-            user.save(function (_) { return res.json(family); });
+            user.save(_ => res.json(family));
         });
     });
 });
-exports.api.get("/families/:id", middleware_1.authorizeToken, function (req, res) {
-    var uid = req.authenticatedUser._id;
+exports.api.get("/families/:id", middleware_1.authorizeToken, (req, res) => {
+    const uid = req.authenticatedUser._id;
     models_1.Family.findById(req.params["id"])
         .populate("members")
-        .exec(function (err, family) {
+        .exec((err, family) => {
         if (err)
             return res.status(500).json(err);
         return res.json(family);
     });
 });
-exports.api.post("/families/:id/join", middleware_1.authorizeToken, function (req, res) {
-    var uid = req.authenticatedUser._id;
-    models_1.Family.findById(req.params["id"], function (err, family) {
+exports.api.post("/families/:id/join", bodyParser.json(), middleware_1.authorizeToken, (req, res) => {
+    const uid = req.authenticatedUser._id;
+    models_1.Family.findById(req.params["id"], (err, family) => {
         if (err)
             return res.status(500).json(err);
         if (family.members.indexOf(uid) > 0)
             return res.status(200);
         family.members.push(uid);
-        family.save(function (_) {
-            models_1.User.findById(uid, function (err, user) {
+        family.save(_ => {
+            models_1.User.findById(uid, (err, user) => {
                 if (err)
                     return res.status(500).json(err);
                 user.families.push(family._id);
-                user.save(function (_) { return res.sendStatus(200); });
+                user.save(_ => res.sendStatus(200));
             });
         });
     });
 });
-exports.api.get("/topics", middleware_1.authorizeToken, function (req, res) {
-    var user = req.authenticatedUser;
+exports.api.get("/topics", middleware_1.authorizeToken, (req, res) => {
+    const user = req.authenticatedUser;
     models_1.Topic.find({
         _family: {
             $in: user.families
         }
-    }, function (err, topics) {
+    }, (err, topics) => {
         if (err)
             res.status(500).json(err);
         res.json(topics);
     });
 });
-exports.api.post("/topics", middleware_1.authorizeToken, function (req, res) {
-    var user = req.authenticatedUser;
+exports.api.post("/topics", bodyParser.json(), middleware_1.authorizeToken, (req, res) => {
+    const user = req.authenticatedUser;
     new models_1.Topic({
         _creator: user._id,
         _family: user.families[0],
         active: true,
         locked: true,
         name: req.body["name"]
-    }).save(function (err, topic) {
+    }).save((err, topic) => {
         if (err)
             return res.status(500).json(err);
         else {
@@ -141,48 +140,78 @@ exports.api.post("/topics", middleware_1.authorizeToken, function (req, res) {
         }
     });
 });
-exports.api.get("/topics/:id", middleware_1.authorizeToken, function (req, res) {
+exports.api.get("/topics/:id", middleware_1.authorizeToken, (req, res) => {
     models_1.Topic.findById(req.params["id"])
         .populate("_creator")
         .populate("_family")
         .populate("images")
-        .exec(function (err, topic) {
+        .exec((err, topic) => {
         if (err)
             return res.status(500).json(err);
         res.json(topic);
     });
 });
-exports.api.put("/topics/:id", middleware_1.authorizeToken, function (req, res) {
+exports.api.put("/topics/:id", bodyParser.json(), middleware_1.authorizeToken, (req, res) => {
     models_1.Topic.findOneAndUpdate({ _id: req.params["id"] }, req.body, { new: true })
-        .exec(function (err, topic) {
+        .exec((err, topic) => {
         if (err)
             return res.status(500).json(err);
         res.json(topic);
     });
 });
-var upload = multer({
+exports.api.get("/topics/:id/participants", middleware_1.authorizeToken, (req, res) => {
+    const topicId = req.params["id"];
+    models_1.Topic.findById(topicId)
+        .exec((err, topic) => {
+        models_1.Family.findById(topic._family)
+            .exec((err, family) => {
+            const userIds = family.members;
+            models_1.Image.find({ _topic: topicId })
+                .exec((err, images) => {
+                const submittedUserIds = images.map((image) => image._creator.toString());
+                const unsubmittedUserIds = userIds.filter((userId) => {
+                    return submittedUserIds.indexOf(userId.toString()) < 0;
+                });
+                const submittedUsers = models_1.User.find({ _id: { $in: submittedUserIds } }).exec();
+                const unsubmittedUsers = models_1.User.find({ _id: { $in: unsubmittedUserIds } }).exec();
+                Promise.all([
+                    submittedUsers, unsubmittedUsers
+                ]).then((responses) => {
+                    res.json({
+                        submitted: responses[0],
+                        not_submitted: responses[1]
+                    });
+                });
+            });
+        });
+    });
+});
+const upload = multer({
+    limits: {
+        fileSize: 1000000
+    },
     storage: multerS3({
         acl: "public-read",
         bucket: "famjam",
-        contentType: function (req, file, cb) {
+        contentType: (req, file, cb) => {
             cb(null, "image/jpeg");
         },
-        key: function (req, file, cb) {
+        key: (req, file, cb) => {
             cb(null, uuid.v4());
         },
         s3: s3
     })
 });
-exports.api.post("/topics/:id", middleware_1.authorizeToken, upload.array("photo", 1), function (req, res) {
+exports.api.post("/topics/:id", middleware_1.authorizeToken, upload.single("photo"), (req, res) => {
     new models_1.Image({
         _creator: req.authenticatedUser._id,
         _topic: req.params["id"],
         description: req.body["description"],
-        url: req.files[0].location
-    }).save(function (err, image) {
-        models_1.Topic.findById(req.params["id"], function (err, topic) {
+        url: req.file.location
+    }).save((err, image) => {
+        models_1.Topic.findById(req.params["id"], (err, topic) => {
             topic.images.push(image._id);
-            topic.save(function (err) {
+            topic.save((err) => {
                 if (err)
                     res.status(401).json(err);
                 else {
--- famjam-server/src/app/db.ts
+++ famjam-server/src/app/db.ts
@@ -3,5 +3,7 @@ import * as mongoose from "mongoose";
 const mongoUri = process.env.MONGODB_URI;
 
 mongoose.connect(mongoUri);
+// HACKHACK: Override with native ES6 promises
+(mongoose as any).Promise = global.Promise;
 
 export const db = mongoose.connection;
