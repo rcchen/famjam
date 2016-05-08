42e5ebdf4bb115c5ac3c80ecd2db3f71eba565f6
Roger Chen <rogerthechen@gmail.com>
2016-05-07 18:33:45 -0700

    Clean up things some more

--- famjam-app/FamJam/FamJam/NewUserViewController.swift
+++ famjam-app/FamJam/FamJam/NewUserViewController.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2016 Accord.io. All rights reserved.
 //
 
+import GoogleMaterialIconFont
 import PromiseKit
 import UIKit
 
@@ -13,15 +14,6 @@ class NewUserViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-        
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardWillShow:"), name: UIKeyboardWillShowNotification, object: nil)
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardWillHide:"), name: UIKeyboardWillHideNotification, object: nil)
-        
-        // Makes it such that tapping out of keyboard will hide it
-        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
-        view.addGestureRecognizer(tap)
     }
 
     override func didReceiveMemoryWarning() {
@@ -32,99 +24,58 @@ class NewUserViewController: UIViewController {
     
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-//        if (segue.identifier == "newUserCreated") {
-//            AnonymousApiService.createUser(usernameTextField.text!, password: passwordTextField.text!, displayName: displaynameTextField.text!, cb: {})
-//            
-//        }
         
     }
-    
-    @IBAction func doneButtonPressed(sender: UIBarButtonItem) {
 
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var displaynameTextField: UITextField!
+    
+    @IBAction func doneButtonPressed(sender: UIButton) {
         let username = usernameTextField.text!
         let password = passwordTextField.text!
         let displayName = displaynameTextField.text!
         let familyName = familyTextField.text!
-
+        
         let authenticatedService = AuthenticatedApiService.sharedInstance
         firstly { () -> Promise<Void> in
             AnonymousApiService.createUser(username, password: password, displayName: displayName)
         }.then { () -> Promise<Void> in
             return AnonymousApiService.authenticateUser(username, password: password)
-        .then { () -> Promise<Family> in
-            authenticatedService.setHeaders()
-            return AuthenticatedApiService.sharedInstance.joinOrCreateFamily(familyName)
-        }.then { family -> Promise<User> in
-            return authenticatedService.getMe()
-        }.then { user -> Promise<[Family]> in
-            AppData.ACTIVE_USER = user
-            return authenticatedService.getMeFamilies()
-        }.then { families -> Promise<[Topic]> in
-            AppData.ACTIVE_FAMILY = families[0]
-            
-            return authenticatedService.getTopics(true)
-            
-        }.then { topics -> Promise<[Topic]> in
-            if (topics.count == 0) {
-                AppData.ACTIVE_TOPIC = nil
-            } else {
-                AppData.ACTIVE_TOPIC = topics[0]
+            .then { () -> Promise<Family> in
+                authenticatedService.setHeaders()
+                return AuthenticatedApiService.sharedInstance.joinOrCreateFamily(familyName)
+            }.then { family -> Promise<User> in
+                return authenticatedService.getMe()
+            }.then { user -> Promise<[Family]> in
+                AppData.ACTIVE_USER = user
+                return authenticatedService.getMeFamilies()
+            }.then { families -> Promise<[Topic]> in
+                AppData.ACTIVE_FAMILY = families[0]
+                
+                return authenticatedService.getTopics(true)
+                
+            }.then { topics -> Promise<[Topic]> in
+                if (topics.count == 0) {
+                    AppData.ACTIVE_TOPIC = nil
+                } else {
+                    AppData.ACTIVE_TOPIC = topics[0]
+                }
+                
+                return authenticatedService.getTopics(nil)
+            }.then { topics -> Promise<[User]> in
+                //AppDataFunctions.addTopicsToAllTopicsArray(topics)
+                AppData.ALL_TOPICS = topics
+                return authenticatedService.getFamilyMembers((AppData.ACTIVE_FAMILY?._id)!)
+            }.then { familyMembers -> Void in
+                AppData.ACTIVE_FAMILY_MEMBERS = familyMembers
+                self.performSegueWithIdentifier("newUserCreated", sender: self)
             }
-            
-            return authenticatedService.getTopics(nil)
-        }.then { topics -> Promise<[User]> in
-            //AppDataFunctions.addTopicsToAllTopicsArray(topics)
-            AppData.ALL_TOPICS = topics
-            return authenticatedService.getFamilyMembers((AppData.ACTIVE_FAMILY?._id)!)
-        }.then { familyMembers -> Void in
-            AppData.ACTIVE_FAMILY_MEMBERS = familyMembers
-            self.performSegueWithIdentifier("newUserCreated", sender: self)
         }
     }
-    
-    
-    }
-    
-    var keyBoardShowing = false
-    
-    @IBOutlet weak var usernameTextField: UITextField!
-    
-    
-    @IBOutlet weak var displaynameTextField: UITextField!
-    
-    
-    @IBOutlet weak var passwordTextField: UITextField!
 
-    
-    @IBOutlet weak var familyTextField: UITextField!
-    
-    
-    // Will show the keyboard
-    func keyboardWillShow(notification: NSNotification) {
-        
-        if (!keyBoardShowing) {
-            if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.CGRectValue() {
-                self.view.frame.origin.y -= keyboardSize.height
-            }
-            
-            keyBoardShowing = true
-        }
-    }
-    
-    // Will hide the keyboard
-    func keyboardWillHide(notification: NSNotification) {
-        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.CGRectValue() {
-            self.view.frame.origin.y += keyboardSize.height
-        }
-        keyBoardShowing = false
-    }
-    
-    //Calls this function when the tap is recognized.
-    func dismissKeyboard() {
-        //Causes the view (or one of its embedded text fields) to resign the first responder status.
-        view.endEditing(true)
-    }
-    
+    weak var familyTextField: UITextField!
+
     /*
     // MARK: - Navigation
 
